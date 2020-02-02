0d1a9bfbca6aa418231251036a310b9a7b20d6a4
TheRoniOne <ronidani99@hotmail.com>
2020-02-02 16:54:59 -0500

    empezando

---
+++ redes.py
@@ -0,0 +1,20 @@
+import csv
+
+class router:
+    def __init__(self, nombre, ip):
+        self.nombre = nombre
+        self.ip = ip
+        self.area = 1
+        self.conocidos = []
+
+    def anunciarVecinos(self):
+        pass
+
+def leerCSV():
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
